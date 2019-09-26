e8dcb75f3dd5ad3b12a5a286aa5dc01ef28454bc
Evgeniy Bessonov <e.bessonov@tinkoff.ru>
2019-09-26 10:17:39 +0300

    test 2

---
+++ README.md
@@ -0,0 +1,13 @@
+# Переменные для разных окружений
+
+Общие переменные заданы в `./group_vars/all.yml`
+
+env
+who
+surname
+
+
+Переменные для окружения `test` заданы в `./test/group_vars/demo.yml`
+
+Параметр `hash_behaviour = merge` в `ansible.cfg` для объединения переменных в виде словарей.
+
--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 echo RUN TEST_ENV
 ansible-playbook -i test/hosts variable_example.yml
 
-echo run qa_env
-ansible-playbook -i qa/hosts variable_example.yml
+# echo run qa_env
+# ansible-playbook -i qa/hosts variable_example.yml
 
-echo run prod_env
-ansible-playbook -i prod/hosts variable_example.yml
+# echo run prod_env
+# ansible-playbook -i prod/hosts variable_example.yml
 
